d0f92a2009bbdbaadd48b2de36a15e2f4aa09035
Teal Hobson-Lowther <teal@squishymedia.com>
2018-05-01 12:03:53 -0700

    correct celery config; upload task started

--- datalad_service/s3.py
+++ datalad_service/s3.py
@@ -1,15 +1,17 @@
-from kombu import Exchange, Queue
+import os
+from .datalad import DataladStore
+import datalad
 from celery import Celery
 
-app = Celery('hello', broker='redis:6379', backend='redis:6379/celery')
-
+app = Celery('s3', broker="redis://redis", backend="redis://redis/0")
 
 @app.task
-def export(datasetId, tag, public):
-    queueId = 'dataset'
-    queues = app.control.inspect().active_queues()
-    return queues
-    
-    
-def create_consumer(queueId):
-    return
+def upload(annex_path, datasetId, tag, public):
+    dataset = DataladStore(annex_path).get_dataset(datasetId)
+
+    # create bucket string
+    bucket_string = 'ssh://' + os.environ['AWS_S3_TEST_BUCKET']
+
+    # create sibling
+    sibling = datalad.api.create_sibling(bucket_string, dataset=dataset)
+    return 'result'
