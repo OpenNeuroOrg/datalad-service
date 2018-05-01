a834a998030b9eaed4e6aabfcd2a6bb3421d44cc
Teal Hobson-Lowther <teal@squishymedia.com>
2018-05-01 12:05:23 -0700

    loose framework of celery async upload test

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,9 +1,9 @@
 import json
 import pytest
+from datalad_service.s3 import upload
+from .dataset_fixtures import *
 
-from datalad_service.s3 import export
-
-
-def test_app():
-    response = export.delay('dsId', 'tag', False)
-    assert response == 'hello'
+def test_celery():
+    annex_path = 'datalad'
+    task = upload.delay(annex_path, DATASET_ID, 'tag', False)
+    assert task is not None
